5acc62032060d1425c08f1af969028e821b1b85d
paragchauhan98 <iamparag121298@gmail.com>
2023-04-02 03:27:20 +0530

    d

--- db.js
+++ db.js
@@ -1,3 +1,15 @@
-const mongoose=require("mongoose")
-mongoose.connect('mongodb+srv://iamparag121298:Manubond07@cluster1.0sgwevf.mongodb.net/blogApp?retryWrites=true&w=majority')
-module.exports=mongoose
+const url = 'mongodb+srv://iamparag121298:Manubond07@cluster1.0sgwevf.mongodb.net/blogApp?retryWrites=true&w=majority';
+
+const mongoose = require('mongoose');
+mongoose.connect(url)
+.then(()=>{
+    console.log('congrats, problem solved')
+})
+.catch((err)=>{
+    console.log(`there is not a problem with ${err.message}`);
+    process.exit(-1)
+})
+
+
+
+module.exports = mongoose;
